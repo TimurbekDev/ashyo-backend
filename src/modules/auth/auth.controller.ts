108abfef87741e779b4ae05806a107175ff41ff2
Nodirbek Savbatov Sherzod O'g'li <116478730+nodir0306@users.noreply.github.com>
2025-01-20 18:10:17 +0500

    updated auth modul

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ForgotPasswordDto, ResetPasswordDto, SignInDto, SignUpDto } from './dto';
 // import { AuthGuard } from '@nestjs/passport';
 
@@ -8,27 +8,30 @@ import { ForgotPasswordDto, ResetPasswordDto, SignInDto, SignUpDto } from './dto
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+@ApiOperation({summary: "Sign up"})
   @Post('sign-up')
   async signUp(@Body() signUpDto:SignUpDto){
     return await this.authService.signUp(signUpDto);
   }
 
+  @ApiOperation({summary: "Sign in"})
   @Post('sign-in')
   async signIn(@Body() signInDto:SignInDto){
     return await this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({summary: "Sign forgot password"})
   @Post('forgot-password')
   async forgotPassword(@Body() forgotPassDto:ForgotPasswordDto){
     return await this.authService.forgotPassword(forgotPassDto.email);
   }
 
+  @ApiOperation({summary: "reset password"})
   @Post('reset-password')
   async resetPassword(@Body() resetPassDto:ResetPasswordDto){
     return await this.authService.resetPassword(resetPassDto);
   }
 
 
-  
+
 }
