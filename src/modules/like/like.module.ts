3c39bccdf10a39d7253d810419f2b48149ff921e
Timurbek <timurbekbeksaburov.com>
2025-01-31 15:04:36 +0500

    like fixed

--- src/modules/like/like.module.ts
+++ src/modules/like/like.module.ts
@@ -6,9 +6,11 @@ import { UserService } from '../user';
 import { CategoryService } from '../category';
 import { UploadService } from '../upload';
 import { ProductItemService } from '../product-item';
+import { ProductService } from '../product';
+import { ColorService } from '../color';
 
 @Module({
   controllers: [LikeController],
-  providers: [LikeService,PrismaService,ProductItemService,UserService,CategoryService,UploadService],
+  providers: [LikeService,PrismaService,ProductItemService,UserService,CategoryService,UploadService,ProductService,ColorService],
 })
 export class LikeModule {}
